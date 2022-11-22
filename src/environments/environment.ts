4ed3183189572c65e3cea9f7fe47ac646cbac826
MartaNaranjoEipperle <111298810+MartaNaranjoEipperle@users.noreply.github.com>
2022-11-22 08:59:30 +0100

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'simple-crm-21859',
+    appId: '1:422417329070:web:3c715f15bad8cc32dab63f',
+    storageBucket: 'simple-crm-21859.appspot.com',
+    apiKey: 'AIzaSyAnlWgohgL6lwfT_OLXd_sEeg3pzB15eYs',
+    authDomain: 'simple-crm-21859.firebaseapp.com',
+    messagingSenderId: '422417329070',
+  },
   production: false
 };
 
